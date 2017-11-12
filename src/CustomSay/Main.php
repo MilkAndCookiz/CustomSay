d686e53ea46a8379b1330dfd3eec278dc3aba741
CookieCode <etienne.crosnier.la@gmail.com>
2017-11-12 11:48:02 +0100

    Update to ALPHA-9

--- src/CustomSay/Main.php
+++ src/CustomSay/Main.php
@@ -26,7 +26,7 @@ class Main extends PluginBase implements Listener{
         $this->getLogger()->info(TextFormat::BLUE . "CustomSay disabled.");
     }
 
-    public function onCommand(CommandSender $sender, Command $command, $label, array $args){
+    public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool{
         switch($command->getName()){
 
           case "customsay":
